b1380b409d8617b9f48e3f7193189134c89ad72f
thiraviyalingesh <lingesh559@gmail.com>
2025-06-03 17:18:21 +0530

    Report module updated

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    #allow_origins=["http://localhost:3000"],
+    allow_origins=["*"], # Allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
